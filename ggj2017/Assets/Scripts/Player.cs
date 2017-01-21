a8fbafa68c1533d96cb7141cd16be424dab55e2c
Du Lin <lnidux@gmail.com>
2017-01-21 08:50:24 +0100

    Player.cs per test

---
+++ ggj2017/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private Transform tr;
+	private float m_horizontal;
+	private float m_vertical;
+	public float m_speed = 100;
+
+	void Start()
+	{
+		tr = GetComponent<Transform>();
+	}
+	
+	void Update ()
+	{
+		m_horizontal = Input.GetAxis ("Horizontal");
+		m_vertical = Input.GetAxis ("Vertical");
+	}
+
+	void FixedUpdate()
+	{
+		tr.position = tr.position + m_horizontal * transform.right * m_speed * Time.fixedDeltaTime + m_vertical * transform.up * m_speed * Time.fixedDeltaTime;
+	}
+}
