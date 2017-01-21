289ce65e30f261b1cefedb18c1e8e194886bb193
Brogni <brognolisimone@gmail.com>
2017-01-21 02:59:07 +0100

    Generating expanding wave

---
+++ ggj2017/Assets/Scripts/SonicWave.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SonicWave : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.gameObject.transform.localScale.x < 20f)
+        {
+            this.gameObject.transform.localScale += new Vector3(0.1f, 0.1f, 0);
+        }
+	}
+}
