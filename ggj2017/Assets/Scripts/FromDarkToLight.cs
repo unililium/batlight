5f58309b96efa900abec1c20e05c84f67df5a62d
Brogni <brognolisimone@gmail.com>
2017-01-22 00:08:11 +0100

    Placcing first landscape

---
+++ ggj2017/Assets/Scripts/FromDarkToLight.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FromDarkToLight : MonoBehaviour {
+
+    private SpriteRenderer sprite;
+
+	// Use this for initialization
+	void Start () {
+        sprite = this.GetComponent<SpriteRenderer>();
+        this.sprite.color = new Color(0.2f, 0.2f, 0.2f); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.sprite.color += new Color(0.01f, 0.01f, 0.01f);
+    }
+}
