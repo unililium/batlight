cff77e6a4758d99c5ce441632af9ac817104e129
Da_Macchi <damacchi91@gmail.com>
2017-01-21 16:56:30 +0100

    sonar ring
    non pago affitto

---
+++ ggj2017/Assets/Scripts/Sonar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sonar : MonoBehaviour {
+	Animator radarAnim;
+	public Animator returnAnim;
+	bool activate = true;
+	// Use this for initialization
+	void Start () {
+		radarAnim = GetComponent<Animator> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("space")) {
+			radarAnim.SetBool ("Activate", activate);
+			returnAnim.SetBool ("ActivateReturn", activate);
+		}
+	}
+}
