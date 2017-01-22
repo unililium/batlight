982d6ca21f8381ec3959ef0dd9af59958379d454
Da_Macchi <damacchi91@gmail.com>
2017-01-22 10:00:27 +0100

    le animazioni sono più belline

---
+++ ggj2017/Assets/Scripts/Wave.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour {
+
+	bool activate = false;
+	// Use this for initialization
+	void Start () {
+		EventManager.StartListening ("Sonar", Animate);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Animator> ().SetBool ("Activate", activate);
+	}
+	void Animate(){
+		activate = true;
+	}
+	void EndAnimate(){
+		activate = false;
+	}
+
+}
