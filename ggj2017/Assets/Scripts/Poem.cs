1017f7f8f826a6cfaee09d2a60e5e2ea8a4dd7ef
Du Lin <lnidux@gmail.com>
2017-01-22 12:24:59 +0100

    poemi

---
+++ ggj2017/Assets/Scripts/Poem.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Poem : MonoBehaviour {
+
+	/// <summary>
+	/// Sent when another object enters a trigger collider attached to this
+	/// object (2D physics only).
+	/// </summary>
+	/// <param name="other">The other Collider2D involved in this collision.</param>
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+            EventManager.TriggerEvent("ReadPoem");
+            // read poem
+		}
+	}
+}
---
+++ ggj2017/Assets/Scripts/PoemText.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoemText : MonoBehaviour {
+
+    void Start()
+    {
+    }
+}
